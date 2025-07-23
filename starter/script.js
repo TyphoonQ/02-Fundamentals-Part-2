6d270b17477e0a3e0223a383864ed44837c456f8
Tyler Quintana <tylerquintana537@gmail.com>
2025-07-23 13:56:05 -0600

    Functions

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,4 @@
+/*
 // 33
 'use strict';
 
@@ -9,3 +10,27 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 //const interface = 'Audio';
 //const private = 534;
+*/
+
+// 34 Functions
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrnageJuice = fruitProcessor(2, 4);
+console.log(appleOrnageJuice);
