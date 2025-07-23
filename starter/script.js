dd57000c58566c5633c70a141bc9db0ae24929c6
Tyler Quintana <tylerquintana537@gmail.com>
2025-07-23 14:29:58 -0600

    Functions Declerations vs Expressions Assignment

--- starter/script.js
+++ starter/script.js
@@ -68,3 +68,17 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+};
+
+const percPortugal1 = percentageOfWorld1(10);
+const percChina1 = percentageOfWorld1(1441);
+const percUSA1 = percentageOfWorld1(332);
+
+console.log(percPortugal1, percChina1, percUSA1);
