3cf4c01f289846e8bec04a012d594431e0098d7e
Tyler Quintana <tylerquintana537@gmail.com>
2025-07-23 14:15:38 -0600

    Functions Assignment

--- starter/script.js
+++ starter/script.js
@@ -10,7 +10,7 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 //const interface = 'Audio';
 //const private = 534;
-*/
+
 
 // 34 Functions
 
@@ -34,3 +34,18 @@ console.log(appleJuice);
 
 const appleOrnageJuice = fruitProcessor(2, 4);
 console.log(appleOrnageJuice);
+
+const num = Number('23');
+*/
+
+function describeCountry(country, population, capitalCity) {
+    console.log(country, population, capitalCity);
+    const location = `${country} has ${population} million people and its capital city is ${capitalCity}.`
+    return location;
+}
+
+const descPortugal = describeCountry('Portugal', 10, 'Lisbon');
+const descGermany = describeCountry('Germany', 83, 'Berlin');
+const descFinland = describeCountry('Finland', 6, 'Helsinki');
+
+console.log(descPortugal, descGermany, descFinland);
